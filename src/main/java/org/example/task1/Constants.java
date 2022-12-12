22def91e9063db038c09354a9548bd23988292a7
Anna Bocharova <120383629+annabocharovaua@users.noreply.github.com>
2022-12-12 15:15:39 +0200

    Delete src directory

--- src/main/java/org/example/task1/Constants.java
+++
@@ -1,8 +0,0 @@
-package org.example.task1;
-
-public final class Constants {
-    private Constants() {}
-
-    public static final int PORT = 8888;
-    public static final String HOST = "localhost";
-}
